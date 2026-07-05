be9ec74674b71229af2e1eb85a6472d14bb2c7bc
Eman Izhar <166126055+Eizhar2005@users.noreply.github.com>
2026-07-05 10:41:25 +0500

    Implement user registration with JWT authentication and bcrypt hashing

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,9 +1,42 @@
 const userModel = require("../models/user.models");
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
 
 async function resgisterUser(req, res) {
- 
-        const {username, email, password, role='user'}  = req.body;
+  const { username, email, password, role = "user" } = req.body;
 
+  const isUserALreadyExists = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (isUserALreadyExists) {
+    return res.status(409).json({
+      message: "user already exists",
+    });
+  }
+  const hash = await bcrypt.hash(password, 10)
+
+const user = await userModel.create({
+        username,
+        email,
+        password:hash,
+        role
+})
+const token = jwt.sign({ //jwt.sign() is used to generate a token
+        id:user._id,
+        role: user.role
+}, process.env.JWT_SECRET)
+
+res.cookie("token", token) //cookie is set in the browser
+
+res.status(201).json({
+        message:"user registered successfully",
+        user:{
+                id:user._id,
+                username: user.username,
+                email:user.email,
+                role: user.role,
+        }
+})
 }
 
 module.exports = { resgisterUser };
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,8 +1,7 @@
 const express = require("express")
-const { resgisterUser } = require("../controllers/auth.controller");
-
+const authController = require("../controllers/auth.controller")
 const router = express.Router();
 
-router.post('/register', resgisterUser)
+router.post('/register', authController.resgisterUser )
 
 module.exports = router;
